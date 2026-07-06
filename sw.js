a32d837d85e700432bfebad1b170e7ea8f44e106
melnwsc2-cmyk <melnwsc2@gmail.com>
2026-07-06 12:14:03 +0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,32 @@
+const CACHE = 'tresuno-v3';
+const ASSETS = [
+  '/tresuno/',
+  '/tresuno/index.html',
+];
+ 
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting())
+  );
+});
+ 
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
+});
+ 
+self.addEventListener('fetch', e => {
+  // Network first strategy - always try network first
+  e.respondWith(
+    fetch(e.request).then(res => {
+      if(res && res.status === 200) {
+        const clone = res.clone();
+        caches.open(CACHE).then(c => c.put(e.request, clone));
+      }
+      return res;
+    }).catch(() => caches.match(e.request))
+  );
+});
